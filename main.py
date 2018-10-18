c0b807b9a3b4d3857d132ab2e12b37751d0becd6
Feer <feergarcia0209@gmail.com>
2018-10-18 16:02:53 -0500

    Agrego herencia en html

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ Created on Mon Oct  8 13:17:43 2018
 from flask import Flask, render_template, request, redirect,flash,url_for
 from flask_pymongo import PyMongo
 import os
+import flask_fs as fs
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'audios'
 app.config["MONGO_URI"] = 'mongodb://Feer:sistemas123@ds125953.mlab.com:25953/audios'
 mongo = PyMongo(app)
 new = mongo.db.archaudio
-
+audio = fs.Storage('audio')
 @app.route('/')
 def index():
       title = "Audio Forense"
@@ -21,19 +22,27 @@ def index():
 
 @app.route('/insert')
 def insert():
-      return render_template('agregar.html')
+      title="Add Audio"
+      return render_template('agregar.html',title=title)
       
 @app.route('/upload', methods=['POST'])
 def upload():
       file=request.files['file']
-      new.insert({'name' : file.filename, 'data' : file.read()}) 
+      new.insert({'usuario' : request.form['user'],'tipo_audio' : request.form['opcion'], 'area_geo' : request.form['lugar'],'name_arch' : file.filename,'data' : file.read()}) 
+
+      #audio.write('my.file',file.read())
       return redirect(url_for('insert'))
 
 @app.route('/lista')
 def lista():
-      title="Lista de audios"
+      title="Audio list"
       res = new.find()
       return render_template('lista.html',title=title,res = res)
+
+@app.route('/acerca')
+def acerca():
+      title="About"
+      return render_template('acerca.html',title=title)
 
 @app.route('/obs')
 def obs():
